a563b7f78366c6610f24f98dc5afcb40acf0c193
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2022-02-07 20:43:46 +0500

    Time: 53 ms (24.32%), Space: 14 MB (87.47%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,24 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # s =""
         if not s or len(s) == 0:
-            return True
-        # (  )       ( [ { } ] )    [   )   ) { }
-        stack = [] #O(n)
-        for c in s:    #O(n)
-            if c =='('or c =='{' or c =='[':
+            return true
+        
+        stack = []
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
                 stack.append(c)
-            elif c ==')' and (len(stack) == 0 or stack[-1] != '('):
+            elif c == ')' and (len(stack) == 0 or stack[-1] !='('):
                 return False
-            elif c =='}' and (len(stack) == 0 or stack[-1] != '{'):
+            elif c == '}' and (len(stack) == 0 or stack[-1] !='{'):
                 return False
-            elif c ==']' and (len(stack) == 0 or stack[-1] != '['):
+            elif c == ']' and (len(stack) == 0 or stack[-1] !='['):
                 return False
             else:
                 stack.pop()
-                
         if len(stack) > 0:
             return False
         
         return True
+            
+            
+            
+            
+        
+        
             
