3c1b23eb8ce9e63a267a63af995a7e0f39460c2b
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2022-03-19 15:15:05 +0500

    Time: 235 ms (29.66%), Space: 16.8 MB (19.42%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -3,10 +3,12 @@ class Solution:
         visited = {}
         for n in nums:
             if n not in visited:
-                visited[n] = 1
+                visited[n] =  1  #{ 2: 1   }
             else:
-                visited[n] += 1
+                visited[n] += 1   #{ 2: 2  } 
         for n in nums:
-            if visited[n] ==1:
+            if visited[n] == 1:
                 return n
+            
+            
         
