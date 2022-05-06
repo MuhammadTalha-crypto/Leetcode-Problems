b131357c4554181ca402f2f7b8e7dc253f36753e
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2022-05-06 23:11:05 +0500

    Time: 42 ms (50.91%), Space: 13.9 MB (11.76%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        count = 0
+        para = 0
+        for i in s:
+            if i == "(":
+                count +=1
+                if para < count:
+                    para = count
+            elif i == ")":
+                count -=1
+                
+        return para
+        
