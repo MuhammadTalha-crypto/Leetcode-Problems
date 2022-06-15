802d00001b3a75e8d6e21db50d3c996e6bcfdc28
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2022-06-15 07:59:13 +0500

    Time: 34 ms (84.17%), Space: 13.9 MB (23.31%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,20 +5,24 @@ class Solution:
         
         stack = []
         for c in s:
-            if c == '(' or c == '{' or c == '[':
+            if c =='(' or c =='{' or c == '[':
                 stack.append(c)
-            elif c == ')' and (len(stack) == 0 or stack[-1] !='('):
+            elif c == ')' and (len(stack) == 0 or stack[-1] != '('):
                 return False
-            elif c == '}' and (len(stack) == 0 or stack[-1] !='{'):
+            elif c == '}' and (len(stack) == 0 or stack[-1] != '{'):
                 return False
-            elif c == ']' and (len(stack) == 0 or stack[-1] !='['):
+            elif c == ']' and (len(stack) == 0 or stack[-1] != '['):
                 return False
             else:
                 stack.pop()
         if len(stack) > 0:
             return False
-        
         return True
+            
+                
+        
+        
+        
             
             
             
