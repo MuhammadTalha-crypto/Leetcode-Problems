9c8067567b626deebe710b98bb870bcd468ab979
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2023-01-10 01:22:59 +0500

    Time: 29 ms (96.99%), Space: 14 MB (47.80%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,10 +3,10 @@ class Solution:
         short = min(strs, key=len) # 
         for item in strs: # When item = "flight"
             while len(short) > 0:
-                if item.startswith(short): # during loop 1 condition fails, during loop 2 condition fails, during loop 3 "flight" startswith fl is True
+                if item.startswith(short):# during loop 1 condition fails, during loop 2 condition fails, during loop 3 "flight" startswith fl is True
                     break
                 else:
-                    short = short[:-1] # during loop 1 short = flo, during loop 2 short = fl
+                    short = short[:-1]# during loop 1 short = flo, during loop 2 short = fl
         return short
 
         
