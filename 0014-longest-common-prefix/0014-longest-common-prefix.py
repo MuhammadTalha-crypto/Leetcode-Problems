f4847853b0dc188d983ef9d384e96fcf5ec32584
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2023-01-10 01:20:49 +0500

    Time: 28 ms (97.70%), Space: 14 MB (10.80%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        short = min(strs, key=len) # 
+        for item in strs: # When item = "flight"
+            while len(short) > 0:
+                if item.startswith(short): # during loop 1 condition fails, during loop 2 condition fails, during loop 3 "flight" startswith fl is True
+                    break
+                else:
+                    short = short[:-1] # during loop 1 short = flo, during loop 2 short = fl
+        return short
+
+        
