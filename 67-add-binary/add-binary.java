711094b3b3520874ad2fda190092757665140888
Muhammad Talha <56351584+MuhammadTalha-crypto@users.noreply.github.com>
2025-10-29 19:02:41 -0700

    Time: 6 ms (9.54%) | Memory: 45.5 MB (5.69%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger two = BigInteger.valueOf(2);
+
+        BigInteger xa = BigInteger.ZERO;
+        for (int i = a.length() - 1, p = 0; i >= 0; --i, ++p)
+            if (a.charAt(i) == '1') xa = xa.add(two.pow(p));
+
+        BigInteger yb = BigInteger.ZERO;
+        for (int j = b.length() - 1, p = 0; j >= 0; --j, ++p)
+            if (b.charAt(j) == '1') yb = yb.add(two.pow(p));
+
+        return xa.add(yb).toString(2);
+    }
+}
